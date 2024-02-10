63c98f20b261c508d52a8e8076470cc14146bf0a
Royi Aizenstain <Royi.aizen@gmail.com>
2024-02-10 21:31:50 +0200

    HW4-q2 Done

--- week_4/hw4- question files/q2.py
+++ week_4/hw4- question files/q2.py
@@ -6,12 +6,40 @@ def estate_homogeneity(divisions, total_area):
 
 def rec_estate_homogeneity(divisions, total_area, counter):
     if isinstance(divisions, int):
-        if total_area == divisions:
-            return True
-        else:
-            return False
-    len_of_list = len(divisions)
-    rec_estate_homogeneity(divisions[counter], total_area / len_of_list, counter)
-    rec_estate_homogeneity(divisions, total_area / len_of_list, counter+1)
+        divisions = [divisions]
+    if not divisions:
+        divisions.append(0)
+    if rec_all_numbers(divisions, 0):
+        return rec_all_equal(divisions, total_area, 0)
+    if counter == len(divisions):
+        return True
+    return rec_estate_homogeneity(divisions[counter], total_area / len(divisions), 0) and rec_estate_homogeneity(
+        divisions, total_area, counter + 1)
+
+    pass
 
 
+def rec_all_numbers(mylist, counter):
+    length = len(mylist)
+    if counter == length:
+        return True
+    if isinstance(mylist[counter], list):
+        return False
+    if counter < length:
+        counter += 1
+        return rec_all_numbers(mylist, counter)
+
+
+def rec_all_equal(mylist, total_area, counter):
+    length = len(mylist)
+    area_each = total_area / length
+    if counter == length:
+        return True
+    if mylist[counter] != area_each:
+        return False
+    if counter < length:
+        counter += 1
+        return rec_all_equal(mylist, total_area, counter)
+
+
+print(estate_homogeneity([100,100,100], 300))
--- week_4/hw4- question files/test.py
+++ week_4/hw4- question files/test.py
@@ -17,8 +17,8 @@ def test_q1():
     output_false5 = q1.word_reconstruction("royi", ["o", "r", "o", "y"])
     output_false6 = q1.word_reconstruction("r", [""])
     all_true = output_true1 and output_true2 and output_true3 and output_true4 and output_true5 and output_true6 and output_true7
-    all_false = output_false1 or output_false2 or output_false3 or output_false4 or output_false5 or output_false6
-    if all_true and not all_false:
+    all_false = not (output_false1 or output_false2 or output_false3 or output_false4 or output_false5 or output_false6)
+    if all_true and all_false:
         print("q1-all tests passed")
     else:
         print("q1-not all tests passed")
@@ -27,15 +27,18 @@ def test_q1():
 def test_q2():
     output_true1 = q2.estate_homogeneity([300, [100, [50, 50], [25, 25, 25, 25]]], 600)
     output_true2 = q2.estate_homogeneity([[50, 50], 100], 200)
+    output_true3 = q2.estate_homogeneity([[], [[]], []], 0)
+    output_true4 = q2.estate_homogeneity([100, 100, 100], 300)
     output_false1 = q2.estate_homogeneity([[50, 50], 100, [50, [10, 20, 20]]], 300)
     output_false2 = q2.estate_homogeneity([[50, 50], 100, [50, [20, 10, 20]]], 300)
-    all_true = output_true1 and output_true2
-    all_false = output_false1 or output_false2
-    if all_true and not all_false:
+    output_false3 = q2.estate_homogeneity([[], [[]], []], 100)
+    output_false4 = q2.estate_homogeneity([100, 100, 50, 50], 300)
+    all_true = output_true1 and output_true2 and output_true3 and output_true4
+    all_false = not (output_false1 or output_false2 or output_false3 or output_false4)
+    if all_true and all_false:
         print("q2-all tests passed")
     else:
         print("q2-not all tests passed")
-
 
 
 def test_q3():
